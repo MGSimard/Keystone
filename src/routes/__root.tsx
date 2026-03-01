5ef34b5684c791d197a171c9de4ef0a2cfff4145
MGSimard <mgsimard.dev@gmail.com>
2026-03-01 18:06:21 -0500

    Cleanup

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -5,15 +5,8 @@ import {
 } from '@tanstack/react-router'
 import { TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
 import { TanStackDevtools } from '@tanstack/react-devtools'
-import Footer from '../components/Footer'
-import Header from '../components/Header'
-
-import StoreDevtools from '../lib/demo-store-devtools'
-
 import TanStackQueryProvider from '../integrations/tanstack-query/root-provider'
-
 import TanStackQueryDevtools from '../integrations/tanstack-query/devtools'
-
 import appCss from '../styles.css?url'
 
 import type { QueryClient } from '@tanstack/react-query'
