74fee86e1eb17bbb538b5bd1c1423b606eca22a2
MGSimard <mgsimard.dev@gmail.com>
2026-03-08 20:41:37 -0400

    Wrap app in themeprovider

--- src/router.tsx
+++ src/router.tsx
@@ -1,6 +1,7 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router";
 import { setupRouterSsrQueryIntegration } from "@tanstack/react-router-ssr-query";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ThemeProvider } from "@/components/ThemeProvider";
 import { routeTree } from "./routeTree.gen";
 
 export function getRouter() {
@@ -16,7 +17,9 @@ export function getRouter() {
     defaultErrorComponent: (props: { error: Error }) => <div>Error: {JSON.stringify(props.error)}</div>,
     defaultNotFoundComponent: () => <div>Not Found</div>,
     Wrap: ({ children }: { children: React.ReactNode }) => (
-      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      <ThemeProvider defaultTheme="system">
+        <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+      </ThemeProvider>
     ),
   });
 
