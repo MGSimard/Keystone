549869dd771977d28726099245efbb29ab0d7dbc
MGSimard <mgsimard.dev@gmail.com>
2026-03-16 05:11:28 -0400

    playing around with adjustments

--- src/routes/_landing/index.tsx
+++ src/routes/_landing/index.tsx
@@ -73,29 +73,29 @@ function PageHome() {
 function HeroSection() {
   return (
     <section className="flex min-h-dvh flex-col">
-      <div className="grid grow grid-cols-1 md:grid-cols-[minmax(0,35%)_minmax(0,1fr)]">
-        <div className="flex flex-col justify-between gap-16 border-r px-6 pt-32 pb-20 md:px-16">
+      <div className="grid grow grid-cols-1 md:grid-cols-[minmax(0,40%)_minmax(0,1fr)]">
+        <div className="flex flex-col justify-center gap-16 px-6 pt-32 pb-20 md:px-16">
           <div>
             <span className="mb-4 block w-fit bg-landing-primary px-2 py-0.5 font-mono text-landing-background">
               TAG LABEL
             </span>
             <h1 className="text-[clamp(2rem,4vw,4rem)]/tight font-bold tracking-tighter uppercase">
-              LOREM IPSUM DOLOR SIT AMET CONSECTETUR
+              LOREM IPSUM DOLOR SIT AMET
             </h1>
           </div>
           <div className="grid gap-6 text-lg">
-            <p className="text-muted-foreground">
+            <p className="text-balance text-muted-foreground">
               Cras dictum dolor vel eros congue, eget commodo dolor bibendum. Ut congue, est id varius congue, purus
               magna eleifend risus, et ullamcorper orci dui ut dui.
             </p>
-            <p>Etiam sed risus nec nibh scelerisque ullamcorper vitae id nisi.</p>
+            <p className="text-balance">Etiam sed risus nec nibh scelerisque ullamcorper vitae id nisi.</p>
             <div className="flex gap-4 font-mono *:flex *:h-12 *:items-center *:justify-center *:px-4 *:py-2">
               <Link to="/" className="bg-landing-primary font-mono text-landing-primary-foreground">
-                GO ROGUE
+                PRIMARY
               </Link>
               <Link
                 to="/"
-                className="text-landing-primary hover:text-landing-primary hover:underline focus-visible:text-landing-primary focus-visible:underline">
+                className="text-sm text-muted-foreground hover:text-landing-primary hover:underline focus-visible:text-landing-primary focus-visible:underline">
                 LEARN MORE &gt;&gt;&gt;
               </Link>
             </div>
