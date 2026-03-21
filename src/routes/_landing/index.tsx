a7cec401b6bee103f299c86d80f7b42f64a7cd13
MGSimard <mgsimard.dev@gmail.com>
2026-03-21 13:34:09 -0400

    Clean up old comments

--- src/routes/_landing/index.tsx
+++ src/routes/_landing/index.tsx
@@ -8,54 +8,6 @@ export const Route = createFileRoute("/_landing/")({
   component: PageHome,
 });
 
-// STYLIZATION NOTES
-// Cool idea: Solid hero background color (or bottom strip) and dither it out in square/circled glyphs into the website's background
-
-// Cool combo
-// 100: #E7E7E6
-// 200: #CFCBBF
-// 300: #B8B19C
-// 400: #4E4A3E
-// 500: #1F2022
-// Y-100: #F5D200
-
-/** LIGHT SETS
- * OFFWHITE BACKGROUND, WASHED OUT DARK GREEN FOREGROUND
- * Background: #f0f1ef - oklch(0.9568 0.0029 128.49)
- * Foreground: #2c3126 - oklch(0.3046 0.0204 127.49)
- *
- * ORANGE BACKGROUND, DARK BROWN FOREGROUND (Optically black)
- * Background: #ff5c01 - oklch(0.6837 0.212 40.54)
- * Foreground: #221715 - oklch(0.2179 0.0184 30.34)
- *
- * ORANGE BACKGROUND, WHITE FOREGROUND
- * Background: #ff781e - oklch(0.7209 0.1875 47.68)
- * Foreground: #ffffff - oklch(1 0 0)
- *
- * OFFWHITE BACKGROUND, DARK GRAY FOREGROUND (Optically black)
- * Background: #f1f1ef - oklch(0.9576 0.0027 106.45)
- * Foreground: #242424 - oklch(0.2603 0 0)
- * Decor: #ff781e - oklch(0.7209 0.1875 47.68)
- *
- * REDDISH ORANGE BACKGROUND, DARK GRAY FOREGROUND
- * Background: #ff5133 - oklch(0.674 0.215 32.24)
- * Foreground: #2d2d2d - oklch(0.2972 0 0)
- * Decor: #f1f2ef - oklch(0.9596 0.0041 121.56)
- */
-
-/** DARK SETS
- *
- * DARK GRAY BACKGROUND, LIGHT GRAY FOREGROUND
- * Background: #191b19 - oklch(0.2192 0.0049 145.41)
- * Foreground: #a4a9a5 - oklch(0.7296 0.008 151.84) (#fff also possible)
- *
- * DARK GRAY BACKGROUND, OFFWHITE & REDDISH ORANGE FOREGROUND
- * Background: #2d2d2d - oklch(0.2972 0 0)
- * Foreground: #f1f2ef - oklch(0.9596 0.0041 121.56)
- * Foreground2: #ff5133 - oklch(0.674 0.215 32.24)
- * Muted: #9ea7a6 - oklch(0.7209 0.0103 188.96) (or opacity from foreground?)
- */
-
 function PageHome() {
   return (
     <>
